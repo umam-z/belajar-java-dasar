9061687300a22af645376e5db55d0bd6a5142480
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-04 23:52:26 +0700

    Tipe Data Char

---
+++ src/TipeDataChar.java
@@ -0,0 +1,13 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char u = 'U';
+        char m = 'M';
+        char a = 'A';
+        char m2 = 'M';
+
+        System.out.print(u);
+        System.out.print(m);
+        System.out.print(a);
+        System.out.print(m2);
+    }
+}
