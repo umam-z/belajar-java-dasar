d71ba442f7b2ec2c3609a09f366821fda5327772
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-05 00:44:18 +0700

    Variable

---
+++ src/Variable.java
@@ -0,0 +1,30 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+
+        name = "Moh. Sirajul Umam";
+
+        System.out.println(name);
+        
+        int age = 25;
+        String address = "Sumenep";
+
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "Ahmad Mustafa";
+        System.out.println(name);
+
+        var firstName = "Moh.";
+        var middleName = "Sirajul";
+        var lastName = "Umam";
+
+        var height = 121;
+
+        // var berat; #e rror
+
+        final String application = "Belajar Java Dasar";
+
+        // application = "Tidak Bisa di Ubah"; # error
+    }
+}
