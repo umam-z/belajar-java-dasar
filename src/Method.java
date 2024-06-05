e7f0408c33fbd15a1f9b7c1b6bf21c507cb66ac4
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-05 19:01:11 +0700

    Method

---
+++ src/Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    public static void main(String[] args) {
+        sayHello();
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
+        System.out.println("Hello World 3");
+    }
+}
