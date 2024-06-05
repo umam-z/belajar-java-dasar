13540ccfbe3bf4ad8a0163071e3e983eded3b47c
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-05 07:10:29 +0700

    Operasi Matematika

---
+++ src/OperasiMatematika.java
@@ -0,0 +1,34 @@
+public class OperasiMatematika {
+    public static void main(String[] args) {
+        byte x = 100;
+        byte y = 10;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x / y);
+        System.out.println(x * y);
+        System.out.println(x % y);
+
+        short z = 100;
+        
+        z += 10;
+        System.out.println(z);
+
+        z /= 10;
+        System.out.println(z);
+
+        z *= 10;
+        System.out.println(z);
+
+        z -= 10;
+        System.out.println(z);
+
+        int d = 100;
+
+        d++;
+        System.out.println(d);
+
+        d--;
+        System.out.println(d);
+    }
+}
