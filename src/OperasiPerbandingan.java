a22afd91e9c80a4c2c52f1cfa0a345271eaf85ef
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-05 07:23:07 +0700

    Operasi Perbandingan

---
+++ src/OperasiPerbandingan.java
@@ -0,0 +1,13 @@
+public class OperasiPerbandingan {
+    public static void main(String[] args) {
+        byte x = 100;
+        byte y = 100;
+
+        System.out.println(x > y);
+        System.out.println(x < y);
+        System.out.println(x <= y);
+        System.out.println(x >= y);
+        System.out.println(x != y);
+        System.out.println(x == y);
+    }
+}
