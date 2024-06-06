16b94463993dddb57a3968440961fdc2b7a8d930
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-06 09:12:35 +0700

    Komentar

---
+++ src/Komentar.java
@@ -0,0 +1,15 @@
+public class Komentar {
+    public static void main(String[] args) {
+        System.out.println(sum(10, 10));
+    }
+    /**
+     * Method untuk menjumlahkan bil1 dan bil2
+     * @param bil1 angka pertama
+     * @param bil2 angka kedua
+     * @return hasil penjumlahan bil1 dan bil2
+     */
+    static int sum(int bil1, int bil2) {
+        // hitung bil1 ditambah bil2
+        return bil1 + bil2;
+    }
+}
