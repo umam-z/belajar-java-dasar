b75ccfbcf36c9074f3542b14f84b6330d18e6744
Laptop Kentang <sirajul.umam6@gmail.com>
2026-01-19 05:08:16 +0700

    tipe data number

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,18 @@
+public class TipeDataNumber {
+    public static void main(String[] args){
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 10000000;
+        long iniLong = 1000000000;
+        long iniLong2 = 1000000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        int decimalInt = 34;
+        int hexaDecimal = 0xffffff;
+        int binaryDecimal = 0b10101001;
+
+        int amount = 1_000_000_000;
+    }
+}
