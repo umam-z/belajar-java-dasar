737a8bfd152cdab1a6b4e9aab62617fb1d85104a
Laptop Kentang <sirajul.umam6@gmail.com>
2026-01-19 05:23:07 +0700

    tipe data char

---
+++ src/TipeDataChar.java
@@ -0,0 +1,16 @@
+public class TipeDataChar {
+    public static void main(String[] args){
+        char f = 'F';
+        char u = 'U';
+        char l = 'L';
+        char a = 'A';
+        char n = 'N';
+
+        System.out.print(f);
+        System.out.print(u);
+        System.out.print(l);
+        System.out.print(a);
+        System.out.print(n);
+
+    }
+}
