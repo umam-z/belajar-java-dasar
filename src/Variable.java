a29fe642353d368e7801eb6d7ffe013fe763b3ff
Laptop Kentang <sirajul.umam6@gmail.com>
2026-01-19 20:49:03 +0700

    variable

---
+++ src/Variable.java
@@ -0,0 +1,24 @@
+public class Variable {
+    public static void main(String[] args){
+        String name;
+        name = "Moh. Sirajul Umam";
+
+        System.out.println(name);
+
+        int age = 26;
+        String address = "Indonesia";
+
+        System.out.println(age);
+        System.out.println(address);
+
+
+        name = "Budi Nugraha";
+        System.out.println(name);
+
+        var firstName = "Moh.";
+        var middleName = "Sirajul";
+        var lastName = "Umam";
+
+        final String application = "Belaar Java Dasar";
+    }
+}
