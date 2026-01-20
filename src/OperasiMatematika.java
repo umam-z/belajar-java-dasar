ddb2bdc8b1d6aa4480efcf62fa543061f110c124
Laptop Kentang <sirajul.umam6@gmail.com>
2026-01-20 10:23:31 +0700

    operasi matematika

---
+++ src/OperasiMatematika.java
@@ -0,0 +1,31 @@
+public class OperasiMatematika{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        int c = 100;
+
+        c += 10;
+        System.out.println(c);
+
+        c -= 10;
+        System.out.println(c);
+
+        c *= 10;
+        System.out.println(c);
+
+        int d = 100;
+
+        d++;
+        System.out.println(d);
+
+        d--;
+        System.out.println(d);
+    }
+}
