cb06379e5b0b175dfb39cabed01cb8d5e731acd3
Laptop Kentang <sirajul.umam6@gmail.com>
2026-01-21 10:55:20 +0700

    Perulangan while dan do while

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,10 @@
+public class DoWhileLoop {
+    public static void main(String[] args){
+        var counter = 1;
+
+        do {
+            System.out.println("Perulangan ke " + counter);
+            counter = counter + 1;
+        } while(  counter <= 10);
+    }
+}
